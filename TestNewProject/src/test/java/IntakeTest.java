aea30068feaa3a631fec50f8c1f9550351d8f55e
Michael Bilinsky <netopya@netopyaplanet.com>
2022-01-15 18:51:14 +0000

    Update unit tests with pcm sim

--- TestNewProject/src/test/java/IntakeTest.java
+++ TestNewProject/src/test/java/IntakeTest.java
@@ -6,6 +6,7 @@ import edu.wpi.first.hal.HAL;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 //import edu.wpi.first.wpilibj.simulation.;
 import edu.wpi.first.wpilibj.simulation.PWMSim;
+import edu.wpi.first.wpilibj.simulation.PCMSim;
 import frc.robot.Intake;
 
 //import frc.robot.Constants.IntakeConstants;
@@ -15,6 +16,7 @@ public class IntakeTest {
   public static final double DELTA = 1e-2; // acceptable deviation range
   Intake intake;
   PWMSim simMotor;
+  PCMSim simPCM;
   //DoubleSolenoidSim simPiston;
 
   @Before // this method will run before each test
@@ -22,6 +24,8 @@ public class IntakeTest {
     assert HAL.initialize(500, 0); // initialize the HAL, crash if failed
     intake = new Intake(); // create our intake
     simMotor = new PWMSim(1); // create our simulation PWM motor controller
+    simPCM = new PCMSim(1);
+    
     //simPiston = new DoubleSolenoidSim(2, 3, 4); // create our simulation solenoid
   }
 
@@ -47,12 +51,16 @@ public class IntakeTest {
   @Test
   public void retractTest() {
     intake.retract();
+    assertFalse("Solenoid1 Fwd", simPCM.getSolenoidOutput(2));
+    assertTrue("Solenoid1 Rwd", simPCM.getSolenoidOutput(3));
     //assertEquals(DoubleSolenoid.Value.kReverse, simPiston.get());
   }
 
   @Test
   public void deployTest() {
     intake.deploy();
+    assertTrue("Solenoid1 Fwd", simPCM.getSolenoidOutput(2));
+    assertFalse("Solenoid1 Rwd", simPCM.getSolenoidOutput(3));
     //assertEquals(DoubleSolenoid.Value.kForward, simPiston.get());
   }
 }
