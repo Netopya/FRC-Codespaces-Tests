b4189e3da49c8a2a7da549f1d00287c6a2923e57
Michael Bilinsky <netopya@netopyaplanet.com>
2022-01-15 16:39:25 +0000

    TestUnitTest

---
+++ TestProject1/src/test/java/frc/robot/RobotTests.java
@@ -0,0 +1,21 @@
+package frc.robot;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class RobotTests {
+
+    Robot robot;
+    @Before
+    void setUp() {
+        //robot = new Robot();
+    }
+
+    @Test
+    void testTheMethod() {
+        //assertTrue(robot.testMethod(7));
+        assertTrue(true);
+    }
+}
